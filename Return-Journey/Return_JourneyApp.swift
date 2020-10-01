9e3bc65483bdac495fa7eb954e534edb944c5f81
Ted <teddsbennett@gmail.com>
2020-10-01 19:05:56 +0100

    Initial Commit

---
+++ Return-Journey/Return_JourneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Return_JourneyApp.swift
+//  Return-Journey
+//
+//  Created by Ted Bennett on 01/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct Return_JourneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
