45e74bb24bccc53fd6d06466f2c41fc749f9ee33
Ted <teddsbennett@gmail.com>
2020-10-03 11:24:34 +0100

    Add Buttons to home view with sign in with apple modal

--- Return-Journey/ContentView.swift
+++ Return-Journey/ContentView.swift
@@ -8,10 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var needSignIn = true
     var body: some View {
-        TabView {
-            UserDeliveryListView().tabItem { Image(systemName: "cube.box.fill")
-                Text("Deliveries")
+        NavigationView {
+            VStack {
+                Spacer()
+                NavigationLink(destination: UserDeliveryListView()) {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 25, style: .continuous)
+                            .fill(Color.purple)
+                            .frame(width: 200, height: 200)
+                        VStack {
+                            Image(systemName: "shippingbox.fill")
+                                .foregroundColor(.white)
+                                .font(.system(size: 100))
+                                .padding(20)
+                            Text("Need a delivery?")
+                                .font(.title2)
+                                .foregroundColor(.white)
+                                .padding(.bottom, 20)
+                        }.frame(width: 200, height: 200)
+                    }
+                }
+                Spacer()
+                ZStack {
+                    RoundedRectangle(cornerRadius: 25, style: .continuous)
+                        .fill(Color.purple)
+                        .frame(width: 200, height: 200)
+                    Image(systemName: "paperplane.fill")
+                        .foregroundColor(.white)
+                        .font(.system(size: 100))
+                        .frame(width: 200, height: 200)
+                }
+                Spacer()
+            }.sheet(isPresented: $needSignIn) {
+                SignInView(presented: $needSignIn)
             }
         }
     }
