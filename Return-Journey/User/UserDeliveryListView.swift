8baeee2a35604b5983e4aa413840cb2904aaf635
Ted <teddsbennett@gmail.com>
2020-10-03 11:24:57 +0100

    Add User Delivery List navigation

--- Return-Journey/User/UserDeliveryListView.swift
+++ Return-Journey/User/UserDeliveryListView.swift
@@ -9,7 +9,14 @@ import SwiftUI
 
 struct UserDeliveryListView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Hello World!")
+        }.navigationTitle("Your Deliveries")
+        .navigationBarItems(
+            trailing: NavigationLink(destination: UserMapView()) {
+                Image(systemName: "plus")
+            }
+        )
     }
 }
 
